16c715846dd4808a8f4fa006d05231b0f6646504
madebycotrim <madebycotrim@gmail.com>
2026-03-11 21:59:45 +0000

    APIIII

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => {
             // Proxy mantido por ser essencial para o desenvolvimento.
             proxy: {
                 '/api': {
-                    target: env.VITE_API|| 'https://api.softhub.workers.dev',
+                    target: env.VITE_API || 'https://api.softhub.workers.dev',
                     changeOrigin: true,
                     secure: false,
                 },
